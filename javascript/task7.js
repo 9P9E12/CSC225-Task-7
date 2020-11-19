6258b74cce9cbf6ea4d95d693a7cdf8875165223
Justin Goodchild <jeg6300@gmail.com>
2020-11-19 00:15:05 -0500

    Implemented returnPrimeNumbers

--- javascript/task7.js
+++ javascript/task7.js
@@ -65,11 +65,27 @@ return numbers;
 function returnPrimeNumbers(){
     const numbers = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15];
     var primes = [];
+    primes = numbers.filter(primeCheck);
+    return primes;
+}
 
+function primeCheck(num){
+    if(num <= 1){
+        return false;
+    } else {
+        for(var i = 2; i < num; i++){
+            if(num % i == 0){
+                return false;
+            }
+        }
+        return true;
+    }
 }
 
 //Write a function that loops through and console.log's the numbers from 1 to 100, except multiples of three, log (without quotes) "CSC225 RULES" instead of the number, for the multiples of five, log (without quotes) "I LOVE JAVASCRIPT". For numbers which are multiples of both three and five, log (without quotes) "CSC225 RULES I LOVE JAVASCRIPT" 
-var result = toCelcius([23, 32, 41, 50, 59]);
+
+var result = returnPrimeNumbers();
 for (let index = 0; index < result.length; index++) {
     console.log(result[index]);
+    
 }
